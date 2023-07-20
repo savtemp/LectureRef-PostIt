00e8b67e379dcd619157be3030e2f267f5aa4d47
Jeremy <jembyfowler@gmail.com>
2023-07-20 14:54:13 -0600

    jeremy is the best

--- RefPostIt.client/src/App.vue
+++ RefPostIt.client/src/App.vue
@@ -5,7 +5,7 @@
   <main>
     <router-view />
   </main>
-   <footer class="bg-dark text-light">
+  <footer class="bg-dark text-light">
     Made with 💖 by CodeWorks
   </footer>
 </template>
@@ -27,7 +27,7 @@ export default {
 <style lang="scss">
 @import "./assets/scss/main.scss";
 
-:root{
+:root {
   --main-height: calc(100vh - 32px - 64px);
 }
 
@@ -36,5 +36,11 @@ footer {
   display: grid;
   place-content: center;
   height: 32px;
+}
+
+main {
+  background-image: url(./assets/img/starbg.png);
+  background-size: cover;
+  background-attachment: fixed;
 }
 </style>
--- RefPostIt.client/src/models/Account.js
+++ RefPostIt.client/src/models/Account.js
@@ -1,9 +1,18 @@
-export class Account {
-  constructor(data) {
+import { DatabaseItem } from "./DatabaseItem.js"
+
+export class Profile extends DatabaseItem {
+  constructor (data) {
+    super(data)
     this.id = data.id
-    this.email = data.email
     this.name = data.name
     this.picture = data.picture
+  }
+}
+
+export class Account extends Profile {
+  constructor (data) {
+    super(data)
+    this.email = data.email
     // TODO add additional properties if needed
   }
 }
